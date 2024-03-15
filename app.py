436711ebb67aecfa0463ce9f6fac5c8e073596e0
Dipen <kalsidipen@gmail.com>
2024-03-16 01:27:35 +0530

    added packages

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
